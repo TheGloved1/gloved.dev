7483de844d518cbe5de20af1000190117161b2ab
TheGloved1 <gloves1229@gmail.com>
2024-05-10 04:44:22 +0000

    update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import "src/styles/globals.css";
 import React from 'react';
 
 export default function HomePage() {
@@ -5,9 +6,8 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e526d] to-[#1e210c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          gloved<span className="text-[hsl(280,100%,40%)]">.</span>dev
+          gloved<span className="text-[hsl(280,100%,40%)]">.</span>dev home
         </h1>
-        <section></section>
       </div>
     </main>
   );
