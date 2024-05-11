e993d6256077af5beb9b55ed0a6e97431c8ef68a
TheGloved1 <gloves1229@gmail.com>
2024-05-11 22:41:55 +0000

    update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function HomePage() {
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
             href="https://gloved.dev/home"
-            target="_blank"
           >
             <h3 className="text-2xl font-bold">Home →</h3>
             <div className="text-lg">
