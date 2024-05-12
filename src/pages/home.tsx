f1393b3b242b22509b911d6155d3c558007074f4
TheGloved1 <gloves1229@gmail.com>
2024-05-12 21:20:51 +0000

    update pages

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,10 +6,12 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e526d] to-[#1e210c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          gloved<span className="text-[hsl(280,100%,40%)]">.</span>dev/home
-          coming soon!
-        </h1>
+        <StyledSection>
+          <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            gloved<span className="text-[hsl(280,100%,40%)]">.</span>dev/home
+            coming soon!
+          </h1>
+        </StyledSection>
         <StyledSection>
           <h3>Welcome</h3>
         </StyledSection>
