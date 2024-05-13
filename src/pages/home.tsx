7e1708ce8c7d3452daead8125a38fe0955533ab9
TheGloved1 <gloves1229@gmail.com>
2024-05-13 14:05:09 +0000

    fix build error

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,34 +7,28 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e526d] to-[#1e210c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <StyledSection>
-          <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            gloved<span className="text-[hsl(280,100%,40%)]">.</span>dev/home
-            work in progress...
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            gloved<span className="text-[hsl(280,100%,40%)]">.</span>dev/home Work in progress...
           </h1>
         </StyledSection>
         <StyledSection>
-          <h1>Welcome to my website!</h1>
-          <p>I'm Kaden Hood</p>
-          <p>A young software engineer</p>
+          <h1>{"Welcome to my website!"}</h1>
+          <p>{"I'm Kaden Hood."}</p>
+          <p>{"I'm a young software engineer."}</p>
         </StyledSection>
         <StyledSection>
-          <h2>About Me</h2>
-          <p></p>
-          <p>I wear gloves, and go by "Gloves" online.</p>
-          <p>and if you couldn't tell already, I like to code.</p>
+          <h2>{"About Me"}</h2>
+          <p>{"I wear gloves, and go by \"Gloves\" online."}</p>
+          <p>{"And if you couldn't tell already, I like to code."}</p>
         </StyledSection>
         <StyledSection>
-          <h2><strong><a className="fancy-link" href="https://meporobotics.com/">Mediapolis High School Robotics
-            Club</a></strong></h2>
-          <p><a className="fancy-link" href="https://frc-events.firstinspires.org/team/9061">FIRST Inspires Robotics Team
-            9061</a></p>
-          <p>Since 2023 - Present</p>
-          <p></p>
-          <p><strong>Role:</strong> Programmer</p>
-          <p><strong>Language:</strong> Java & Python</p>
+          <h2><strong><a className="fancy-link" href="https://meporobotics.com/">{"Mediapolis High School Robotics Club"}</a></strong></h2>
+          <p><a className="fancy-link" href="https://frc-events.firstinspires.org/team/9061">{"FIRST Inspires Robotics Team 9061"}</a></p>
+          <p>{"Since 2023 - Present"}</p>
+          <p><strong>{"Role: "}</strong>{"Programmer"}</p>
+          <p><strong>{"Language: "}</strong>{"Java and Python"}</p>
         </StyledSection>
       </div>
     </main>
   );
-
 }
