bb8195c8a330cb5403e4211dcc4cbb1ea972f35d
TheGloved1 <gloves1229@gmail.com>
2024-05-13 12:21:24 +0000

    update pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function HomePage() {
             </div>
           </Link>
         </div>
-        <div className="grid place-items-center grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+        <div className="grid place-items-center grid-cols-1 gap-4 sm:grid-cols-1 md:gap-8">
           <Link className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20" href="https://old.gloved.dev/">
             <h3 className="text-2xl font-bold">Old Site →</h3>
             <div className="text-lg">
