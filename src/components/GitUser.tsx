2361708aec82edce02d5b65512f9bec4c2ebc060
TheGloved1 <gloves1229@gmail.com>
2024-05-18 04:27:54 +0000

    update

--- src/components/GitUser.tsx
+++ src/components/GitUser.tsx
@@ -1,22 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
 type GitUserProps = {
   name: string
 }
 
-export default async function GitUser({ name }: GitUserProps) {
-  const response = await fetch(`https://api.github.com/users/${name}`)
-  const data = await response.json()
+export default function GitUser({ name }: GitUserProps) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://api.github.com/users/${name}`);
+      const data = await response.json();
+      setData(data);
+    };
+
+    fetchData();
+  }, [name]);
 
   return (
     <div className="container ring ring-white gap-4">
       {data && (
-        <div className="image-container git-image-container">
-          <a href={data.html_url} target="_blank" rel="noopener">
-            <img src={data.avatar_url} alt="User image" />
-          </a>
-        </div>
-      )}
-      {data && (
         <>
+          <div className="image-container git-image-container">
+            <a href={data.html_url} target="_blank" rel="noopener">
+              <img src={data.avatar_url} alt="User image" />
+            </a>
+          </div>
           <h2>{data.name}</h2>
           <span>{data.login}</span>
           <p>{data.bio}</p>
@@ -26,5 +35,5 @@ export default async function GitUser({ name }: GitUserProps) {
         </>
       )}
     </div>
-  )
+  );
 }
