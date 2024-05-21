7cd7820370ca20809102959c9aee3deb5e8aaf12
TheGloved1 <gloves1229@gmail.com>
2024-05-21 05:48:42 +0000

    update

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -6,7 +6,6 @@ export const getUser = async ({ userId }: { userId: string }) => {
   if (!response.ok) {
     throw new Error('Network response was not ok')
   }
-  return await response.json() as object
-
+  return await response.json()
 }
 
