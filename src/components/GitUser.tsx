3dee49750e61ee6123372e453b219e0364672274
TheGloved1 <gloves1229@gmail.com>
2024-05-21 20:25:01 +0000

    use ReactQuery

--- src/components/GitUser.tsx
+++ src/components/GitUser.tsx
@@ -1,7 +1,10 @@
-"use client"
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+import React, {
+  // useEffect,
+  // useState
+} from 'react'
+import { useQuery } from 'react-query'
 
 type UserData = {
   html_url: string
@@ -15,44 +18,35 @@ type GitUserProps = {
   name: string
 }
 
+const getGithubUser = (name: string) => {
+  return useQuery(['user'], async () => {
+    return await (await fetch(`https://api.github.com/users/${name}`)).json() as UserData
+  })
+}
+
 export default function GitUser({ name }: GitUserProps) {
-  const [data, setData] = useState<UserData | null>(null)
+  const data = getGithubUser(name)
   console.log('Rendering GitUser...')
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`https://api.github.com/users/${name}`)
-        const data = await response.json()
-        setData(data)
-      } catch (error) {
-        setData(null)
-        console.error('Error:', error)
-      }
-    }
-
-    void fetchData()
-  }, [])
-
-  if (!data) {
+  if (!data.data) {
     return (
       <div className="container flex flex-col p-4 border-4 gap-4 border-dashed rounded-lx border-white justify-center items-center">
         <p>Loading Data...</p>
       </div>
     )
-  } else if (data) {
+  } else if (data.data) {
     return (
       <div className="container flex flex-col p-4 border-4 gap-4 border-dashed rounded-lx border-white justify-center items-center">
         <div>
-          <Link href={data.html_url} target="_blank" rel="noopener">
-            <Image className="rounded-xl" width={200} height={200} src={data.avatar_url} alt="User image" />
+          <Link href={data.data.html_url} target="_blank" rel="noopener">
+            <Image className="rounded-xl" width={200} height={200} src={data.data.avatar_url} alt="User image" />
           </Link>
         </div>
-        <h2>{data.name}</h2>
-        <span>{data.login}</span>
-        <p>{data.bio}</p>
-        <Link className="fancy-link" href={data.html_url} target="_blank" rel="noopener">
-          {data.html_url}
+        <h2>{data.data.name}</h2>
+        <span>{data.data.login}</span>
+        <p>{data.data.bio}</p>
+        <Link className="fancy-link" href={data.data.html_url} target="_blank" rel="noopener">
+          {data.data.html_url}
         </Link>
       </div>
     )
