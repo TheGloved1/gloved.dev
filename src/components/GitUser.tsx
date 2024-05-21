691c8de003d571abf5823d48f5dbe273f245c227
TheGloved1 <gloves1229@gmail.com>
2024-05-21 04:55:33 +0000

    update

--- src/components/GitUser.tsx
+++ src/components/GitUser.tsx
@@ -1,42 +1,41 @@
 "use client"
-import { githubUser } from '@/server/actions'
 import Image from 'next/image'
 import Link from 'next/link'
-import React, {
-  // useEffect,
-  // useState
-} from 'react'
-import { useQuery } from 'react-query'
+import React, { useEffect, useState } from 'react'
 
-// interface UserData {
-//   html_url: string
-//   avatar_url: string
-//   name: string
-//   login: string
-//   bio: string
-// }
+interface UserData {
+  html_url: string
+  avatar_url: string
+  name: string
+  login: string
+  bio: string
+}
 
 type GitUserProps = {
   name: string
 }
 
 export default function GitUser({ name }: GitUserProps) {
-  const data = useQuery({
-    queryKey: ['user'],
-    queryFn: () => fetch(`https://api.github.com/users/${name}`).then((res) => res.json()),
-  })
+  const [data, setData] = useState<UserData | null>(null)
+  console.log('Rendering GitUser...')
 
-  if (data.isLoading) {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/${name}`).then((res) => res.json()) as UserData
+        setData(response)
+      } catch (error) {
+        console.error('Error:', error)
+      }
+    }
+
+    void fetchData()
+  }, [name])
+
+  if (!data) {
     return (
       <div className="container flex flex-col p-4 border-4 gap-4 border-dashed rounded-lx border-white justify-center items-center">
         <p>Loading Data...</p>
-      </div>
-    )
-  }
-  if (data.isError) {
-    return (
-      <div className="container flex flex-col p-4 border-4 gap-4 border-dashed rounded-lx border-white justify-center items-center">
-        <p>Error fetching data...</p>
       </div>
     )
   }
@@ -46,15 +45,15 @@ export default function GitUser({ name }: GitUserProps) {
       {data && (
         <>
           <div>
-            <Link href={data.data.html_url} target="_blank" rel="noopener">
-              <Image className="rounded-xl" width={200} height={200} src={data.data.avatar_url} alt="User image" />
+            <Link href={data.html_url} target="_blank" rel="noopener">
+              <Image className="rounded-xl" width={200} height={200} src={data.avatar_url} alt="User image" />
             </Link>
           </div>
-          <h2>{data.data.name}</h2>
-          <span>{data.data.login}</span>
-          <p>{data.data.bio}</p>
-          <Link className="fancy-link" href={data.data.html_url} target="_blank" rel="noopener">
-            {data.data.html_url}
+          <h2>{data.name}</h2>
+          <span>{data.login}</span>
+          <p>{data.bio}</p>
+          <Link className="fancy-link" href={data.html_url} target="_blank" rel="noopener">
+            {data.html_url}
           </Link>
         </>
       )}
