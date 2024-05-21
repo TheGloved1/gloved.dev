567a7f735708330d01c81cf259c61b400a4f14a4
TheGloved1 <gloves1229@gmail.com>
2024-05-21 05:33:24 +0000

    update

--- src/components/GitUser.tsx
+++ src/components/GitUser.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 
-interface UserData {
+type UserData = {
   html_url: string
   avatar_url: string
   name: string
@@ -25,6 +25,7 @@ export default function GitUser({ name }: GitUserProps) {
         const response = await fetch(`https://api.github.com/users/${name}`).then((res) => res.json()) as UserData
         setData(response)
       } catch (error) {
+        setData(null)
         console.error('Error:', error)
       }
     }
@@ -38,26 +39,22 @@ export default function GitUser({ name }: GitUserProps) {
         <p>Loading Data...</p>
       </div>
     )
-  }
-
-  return (
-    <div className="container flex flex-col p-4 border-4 gap-4 border-dashed rounded-lx border-white justify-center items-center">
-      {data && (
-        <>
-          <div>
-            <Link href={data.html_url} target="_blank" rel="noopener">
-              <Image className="rounded-xl" width={200} height={200} src={data.avatar_url} alt="User image" />
-            </Link>
-          </div>
-          <h2>{data.name}</h2>
-          <span>{data.login}</span>
-          <p>{data.bio}</p>
-          <Link className="fancy-link" href={data.html_url} target="_blank" rel="noopener">
-            {data.html_url}
+  } else if (data) {
+    return (
+      <div className="container flex flex-col p-4 border-4 gap-4 border-dashed rounded-lx border-white justify-center items-center">
+        <div>
+          <Link href={data.html_url} target="_blank" rel="noopener">
+            <Image className="rounded-xl" width={200} height={200} src={data.avatar_url} alt="User image" />
           </Link>
-        </>
-      )}
-    </div>
-  )
+        </div>
+        <h2>{data.name}</h2>
+        <span>{data.login}</span>
+        <p>{data.bio}</p>
+        <Link className="fancy-link" href={data.html_url} target="_blank" rel="noopener">
+          {data.html_url}
+        </Link>
+      </div>
+    )
+  }
 }
 
