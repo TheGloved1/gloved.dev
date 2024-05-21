d1b4c0ec203f7b2e2080bc19962e77d17e9a4af1
TheGloved1 <gloves1229@gmail.com>
2024-05-21 20:27:08 +0000

    use ReactQuery (correctly)

--- src/components/GitUser.tsx
+++ src/components/GitUser.tsx
@@ -25,28 +25,28 @@ const getGithubUser = (name: string) => {
 }
 
 export default function GitUser({ name }: GitUserProps) {
-  const data = getGithubUser(name)
+  const { data } = getGithubUser(name)
   console.log('Rendering GitUser...')
 
-  if (!data.data) {
+  if (!data) {
     return (
       <div className="container flex flex-col p-4 border-4 gap-4 border-dashed rounded-lx border-white justify-center items-center">
         <p>Loading Data...</p>
       </div>
     )
-  } else if (data.data) {
+  } else if (data) {
     return (
       <div className="container flex flex-col p-4 border-4 gap-4 border-dashed rounded-lx border-white justify-center items-center">
         <div>
-          <Link href={data.data.html_url} target="_blank" rel="noopener">
-            <Image className="rounded-xl" width={200} height={200} src={data.data.avatar_url} alt="User image" />
+          <Link href={data.html_url} target="_blank" rel="noopener">
+            <Image className="rounded-xl" width={200} height={200} src={data.avatar_url} alt="User image" />
           </Link>
         </div>
-        <h2>{data.data.name}</h2>
-        <span>{data.data.login}</span>
-        <p>{data.data.bio}</p>
-        <Link className="fancy-link" href={data.data.html_url} target="_blank" rel="noopener">
-          {data.data.html_url}
+        <h2>{data.name}</h2>
+        <span>{data.login}</span>
+        <p>{data.bio}</p>
+        <Link className="fancy-link" href={data.html_url} target="_blank" rel="noopener">
+          {data.html_url}
         </Link>
       </div>
     )
