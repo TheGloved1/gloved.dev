f2beb9b45fa62496446dbb8973158d227802c62e
TheGloved1 <gloves1229@gmail.com>
2024-05-22 05:52:17 +0000

    adjust section styles

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -24,19 +24,19 @@ export default function Page() {
         gloved<span className="text-[hsl(280,100%,40%)]">.</span>dev<span className="text-[hsl(280,100%,40%)]">/</span>home <span className="text-gray-700">Work in progress...</span>
       </div>
       <div className="flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <StyledSection id={sections[0]} className="snap-center">
+        <StyledSection id={sections[0]} className="snap-start">
           <h3 className="font-extrabold text-xl">{"Welcome to my website!"}</h3>
           <br />
           <p>{"I'm Kaden Hood."}</p>
           <p>{"and I'm a young software engineer."}</p>
         </StyledSection>
-        <StyledSection id={sections[1]} className="snap-center">
+        <StyledSection id={sections[1]} className="snap-start">
           <h3>{"About Me"}</h3>
           <br />
           <p>{"I wear gloves, and go by \"Gloves\" online."}</p>
           <p>{"And if you couldn't tell already, I like to code."}</p>
         </StyledSection>
-        <StyledSection id={sections[2]} className="snap-center">
+        <StyledSection id={sections[2]} className="snap-start">
           <h3><strong><Link className="fancy-link" href="https://meporobotics.com/">{"Mediapolis High School Robotics Club"}</Link></strong></h3>
           <p><Link className="fancy-link" href="https://frc-events.firstinspires.org/team/9061">{"FIRST Inspires Robotics Team 9061"}</Link></p>
           <p>{"Since 2023 - Present"}</p>
@@ -44,7 +44,7 @@ export default function Page() {
           <p><strong>{"Role: "}</strong>{"Programmer"}</p>
           <p><strong>{"Language: "}</strong>{"Java and Python"}</p>
         </StyledSection>
-        <StyledSection id={sections[3]} className="snap-center">
+        <StyledSection id={sections[3]} className="snap-start">
           <h3>{"My Github Profile"}</h3>
           <br />
           <GitUser name="TheGloved1"></GitUser>
