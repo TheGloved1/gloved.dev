835d71f66b65cfc5860c588d79401336a61e88aa
TheGloved1 <gloves1229@gmail.com>
2024-05-22 05:00:59 +0000

    change text size and fix hydration error

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,6 +8,7 @@ import StyledSection from '@/components/StyledSection'
 import { Button } from '@/components/ui/button'
 
 //  TODO: Add more information on how the website is built and things it can do using React.
+//  TODO: Fix the "Home" page to be more responsive and have a better layout.
 
 const sections = ['Welcome', 'About', 'Robotics', 'Github']
 
@@ -19,7 +20,7 @@ export default function Page() {
       <Button className='fixed p-4 top-4 left-4'>
         <Link href={"/"}>{"<- Back"}</Link>
       </Button>
-      <p className="fixed items-center font-bold tracking-tight text-white sm:text-[5rem] animate-pulse">
+      <p className="fixed items-center font-bold tracking-tight text-sm text-white sm:text-[5rem] animate-pulse">
         gloved<span className="text-[hsl(280,100%,40%)]">.</span>dev<span className="text-[hsl(280,100%,40%)]">/</span>home <span className="text-gray-700">Work in progress...</span>
       </p>
       <div className="flex flex-col items-center justify-center gap-12 px-4 py-16 snap-y snap-mandatory">
