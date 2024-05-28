75e03431bd1f7e957c8cdb4dd1c0479dfa9a4a3f
TheGloved1 <gloves1229@gmail.com>
2024-05-28 00:41:37 -0500

    update file uploader

--- src/components/FileManager.tsx
+++ src/components/FileManager.tsx
@@ -106,7 +106,6 @@ export default function FileManager() {
         </ul>
       </div>
       <p className='text-red-500'>{alert}</p>
-      <Button className='rounded-xl' onClick={() => setAlert('This is a test alert!')} title="Test Error">{"Error Test"}</Button>
     </>
   )
 }
