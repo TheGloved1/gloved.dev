5b0ce5b80d824965e50374e576ef6aef26de2e04
TheGloved1 <gloves1229@gmail.com>
2024-05-31 03:33:23 -0500

    update styling

--- src/app/todos/layout.tsx
+++ src/app/todos/layout.tsx
@@ -3,23 +3,16 @@ import { Inter } from "next/font/google"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-
 export const metadata = {
   title: "Todo App",
   description: "A simple todo list web app. Uses local storage to save and get todos list even after reloading.",
 }
 
 
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
+      <body className={`font-sans h-svh w-svw bg-[#333]`}>
         {children}
         <Analytics />
         <SpeedInsights />
