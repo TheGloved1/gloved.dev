a83d6555705b5764faff2243c6f502b1edacfd89
TheGloved1 <gloves1229@gmail.com>
2024-06-02 02:46:10 -0500

    test metadata change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css"
 import { Inter } from "next/font/google"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Head from "next/head"
 
 
 const inter = Inter({
@@ -13,11 +14,15 @@ export const metadata = {
   title: "gloved.dev",
   description: "Made by Kaden Hood. A personal website for my projects and interests. Built using React and hosted with Vercel.",
   icons: [{ rel: "icon", url: "https://avatars.githubusercontent.com/u/96776176?v=4" }],
+  image: "https://avatars.githubusercontent.com/u/96776176?v=4",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:image" content={metadata.image} />
+      </Head>
       <body className={`font-sans ${inter.variable}`}>
         {children}
         <Analytics />
