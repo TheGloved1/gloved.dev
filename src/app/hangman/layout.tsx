4d6417c90b2415d286a27b2b02e7af65d986d6b0
Gloves <gloves1229@gmail.com>
2024-08-30 20:24:09 -0500

    Change page metadata

--- src/app/hangman/layout.tsx
+++ src/app/hangman/layout.tsx
@@ -3,14 +3,14 @@ import '@/styles/globals.css'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: NAME + ' | Todo App',
-  description: 'A simple todo list web app. Uses local storage to save and get todos list even after reloading.',
+  title: NAME + ' | Hangman Game',
+  description: 'A simple hangman game web app. Guess the word. (Might be broken)',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
   return (
     <html lang='en'>
-      <body className={`h-dvh w-dvw bg-[#333] font-sans`}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
