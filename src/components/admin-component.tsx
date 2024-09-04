b037d243395e88e2cec1d7d60440fe951a0aa29c
Gloves <gloves1229@gmail.com>
2024-09-03 22:42:48 -0500

    update AdminComponent

--- src/components/admin-component.tsx
+++ src/components/admin-component.tsx
@@ -4,7 +4,7 @@ import Loading from '@/components/loading'
 import React from 'react'
 
 export default function AdminComponent({ children }: { children: React.ReactNode }): React.JSX.Element | null {
-  const allowedIps = ['207.199.235.110']
+  const allowedIps = ['207.199.235.110', '216.248.102.160']
   const { clientIp, loading } = useFetchIp()
 
   if (loading) {
@@ -12,6 +12,12 @@ export default function AdminComponent({ children }: { children: React.ReactNode
   }
 
   const isAllowed = clientIp !== null && allowedIps.includes(clientIp)
+
+  if (!isAllowed) {
+    console.log('Client IP is not allowed')
+  } else {
+    console.log('Client IP is allowed')
+  }
 
   return isAllowed ? <>{children}</> : null
 }
