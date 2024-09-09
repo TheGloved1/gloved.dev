1d4f04703c60a673a20db3c145ecc18c7df45280
Gloves <gloves1229@gmail.com>
2024-09-08 19:58:06 -0500

    remove unused code

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['zlib-sync', 'node-gyp', 'node-loader'],
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.node/,
-      use: 'node-loader',
-    })
-
-    return config
-  },
   env: {
     NEXT_CLIENT_FILE_MANAGER_PASSKEY: '7693',
   },
