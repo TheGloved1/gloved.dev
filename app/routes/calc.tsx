86ee9df146601c6fc4d7864c264d4f8fda67a7c9
Gloves <gloves1229@gmail.com>
2024-09-12 05:05:25 -0500

    Add meta tags and title to calculator page

--- app/routes/calc.tsx
+++ app/routes/calc.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import CalculatorPage from './calc/CalculatorPage'
+import { NAME } from '@/lib/constants'
+import { MetaFunction } from '@remix-run/react'
+
+export const meta: MetaFunction = () => {
+  return [{ title: NAME + ' | Calculator' }, { name: 'description', content: 'A simple calculator web app. Do math calculations.' }]
+}
 
 export default function Page(): React.JSX.Element {
   return <CalculatorPage />
