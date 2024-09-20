eb47d15b2d9a9712abf1576c56d669e7434c25ce
Gloves <gloves1229@gmail.com>
2024-09-19 20:07:06 -0500

    update Counter.tsx

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Counter() {
-  const [count, setCount] = React.useState(0)
+export default function Counter(): React.JSX.Element {
+  const [count, setCount] = useState(0)
 
   return (
     <div className='justify-center text-center'>
-      <h1>Test React State Button</h1>
-      <button className='btn btn-primary m-4 h-12 w-32 p-4 active:btn-active hover:bg-pink-500 active:bg-pink-700' onClick={() => setCount((c) => c + 1)}>
-        Count is {count}
+      <h1>Test React State Buttons</h1>
+      <h2>Count: {count}</h2>
+      <button className='btn btn-primary m-4 h-12 w-32 p-2 active:btn-active hover:bg-pink-500 active:bg-pink-700' onClick={() => setCount((c) => c + 1)}>
+        Increase Count
       </button>
-      <button className='btn btn-primary m-4 h-12 w-32 p-4 active:btn-active hover:bg-pink-500 active:bg-pink-700' onClick={() => setCount(0)}>
+      <button className='btn btn-primary m-4 h-12 w-32 p-2 active:btn-active hover:bg-pink-500 active:bg-pink-700' onClick={() => setCount((c) => c - 1)}>
+        Decrease Count
+      </button>
+      <button className='btn btn-primary m-4 h-12 w-32 p-2 active:btn-active hover:bg-pink-500 active:bg-pink-700' onClick={() => setCount(0)}>
         Reset Count
       </button>
     </div>
