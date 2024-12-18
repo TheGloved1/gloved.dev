a36b6c07e1d69cebbf2f884754b4d4b1bd6a60f5
TheGloved1 <gloves1229@gmail.com>
2024-12-17 23:33:31 -0600

    create 2 files and update 1 file

---
+++ app/components/ScrollVisibility.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+
+export default function ScrollVisibility({ children, offset }: { children: React.ReactNode; offset?: number }) {
+  const [isAtTop, setIsAtTop] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (offset) {
+        setIsAtTop(window.scrollY === 0 || window.scrollY < offset)
+      } else {
+        setIsAtTop(window.scrollY === 0)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return <>{isAtTop && children}</>
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import AdminComponent from '@/components/AdminComponent'
 import Counter from '@/components/Counter'
+import ScrollVisibility from '@/components/ScrollVisibility'
 import { Black, Calc, Discord, Github, Hangman, Home, NAME, Todos } from '@/lib/constants'
 import type { MetaFunction } from '@remix-run/node'
 import { Link } from '@remix-run/react'
@@ -8,54 +9,98 @@ import React from 'react'
 export const meta: MetaFunction = () => {
   return [
     { title: NAME },
-    { name: 'description', content: 'Made by Kaden Hood. A personal website for my projects and interests. Built using Next.js React Web Framework.' },
+    {
+      name: 'description',
+      content:
+        'Made by Kaden Hood. A personal website for my projects and interests. Built using Next.js React Web Framework.',
+    },
   ]
 }
 
 export default function Page(): React.JSX.Element {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-sky-950 to-[#1e210c] text-white'>
-      <img
-        src='/logo.png'
-        alt=''
-        className='fixed left-1 top-1 hidden h-12 w-12 rounded-xl border-2 border-white shadow-lg sm:left-4 sm:top-4 sm:block sm:h-16 sm:w-16 sm:transform md:left-4 md:top-4 md:h-24 md:w-24 lg:left-4 lg:top-4 xl:left-4 xl:top-4 2xl:left-4 2xl:top-4'
-      />
+      <Link
+        to={'/gallery'}
+        className='fixed bottom-2 left-2 flex flex-row items-center justify-center pl-0 lg:bottom-auto lg:top-2'
+      >
+        <img
+          src='/Leo.png'
+          alt=''
+          className='fixed left-1 top-1 hidden h-12 w-12 rounded-xl border-2 border-white shadow-lg sm:left-4 sm:top-4 sm:block sm:h-16 sm:w-16 sm:transform md:left-4 md:top-4 md:h-24 md:w-24 lg:left-4 lg:top-4 xl:left-4 xl:top-4 2xl:left-4 2xl:top-4'
+        />
+      </Link>
+      <ScrollVisibility offset={100}>
+        <Link
+          to={'/gallery'}
+          className='fixed left-2 top-2 flex items-center justify-center self-center pl-0 sm:hidden lg:hidden'
+        >
+          <button className='rounded bg-blue-500 px-4 py-2 text-white'>Go to Gallery</button>
+        </Link>
+      </ScrollVisibility>
       <div className='container flex flex-col items-center justify-center gap-12 px-4 py-16'>
         <h1 className='text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]'>
           gloved<span className='text-[hsl(280,100%,40%)]'>.</span>dev
         </h1>
         <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8'>
-          <Link className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20' to={Home.link}>
+          <Link
+            className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20'
+            to={Home.link}
+          >
             <h3 className='text-2xl font-bold'>
-              {Home.title} <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>{'->'}</span>
+              {Home.title}{' '}
+              <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+                {'->'}
+              </span>
             </h3>
             <div className='text-lg'>{Home.description}</div>
           </Link>
-          <Link className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20' to={Todos.link}>
+          <Link
+            className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20'
+            to={Todos.link}
+          >
             <h3 className='text-2xl font-bold'>
-              {Todos.title} <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>{'->'}</span>
+              {Todos.title}{' '}
+              <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+                {'->'}
+              </span>
             </h3>
             <div className='text-lg'>{Todos.description}</div>
           </Link>
         </div>
         <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8'>
-          <Link className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20' to={Hangman.link}>
+          <Link
+            className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20'
+            to={Hangman.link}
+          >
             <h3 className='text-2xl font-bold'>
               {Hangman.title} <span className='inline-block transition-transform group-hover:translate-x-1'>{'->'}</span>
             </h3>
             <div className='text-lg'>{Hangman.description}</div>
           </Link>
-          <Link className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20' to={Github.link}>
+          <Link
+            className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20'
+            to={Github.link}
+          >
             <h3 className='text-2xl font-bold'>
-              {Github.title} <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>{'->'}</span>
+              {Github.title}{' '}
+              <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+                {'->'}
+              </span>
             </h3>
             <div className='text-lg'>{Github.description}</div>
           </Link>
         </div>
         <div className='grid grid-cols-1 gap-4 sm:grid-cols-1 md:gap-8'>
-          <Link className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20' to={Calc.link}>
+          <Link
+            className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20'
+            to={Calc.link}
+          >
             <h3 className='text-2xl font-bold'>
-              {Calc.title} <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>{'->'}</span>
+              {Calc.title}{' '}
+              <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+                {'->'}
+              </span>
             </h3>
             <div className='text-lg'>{Calc.description}</div>
           </Link>
@@ -66,15 +111,27 @@ export default function Page(): React.JSX.Element {
             <u>{'Admin Stuff'}</u>
           </h2>
           <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8'>
-            <Link className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20' to={Black.link}>
+            <Link
+              className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20'
+              to={Black.link}
+            >
               <h3 className='text-2xl font-bold'>
-                {Black.title} <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>{'->'}</span>
+                {Black.title}{' '}
+                <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+                  {'->'}
+                </span>
               </h3>
               <div className='text-lg'>{Black.description}</div>
             </Link>
-            <Link className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20' to={Discord.link}>
+            <Link
+              className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20'
+              to={Discord.link}
+            >
               <h3 className='text-2xl font-bold'>
-                {Discord.title} <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>{'->'}</span>
+                {Discord.title}{' '}
+                <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+                  {'->'}
+                </span>
               </h3>
               <div className='text-lg'>{Discord.description}</div>
             </Link>
