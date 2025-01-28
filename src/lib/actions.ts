4e9c4ca068634f7433fc192acd5518c9e314b60e
TheGloved1 <gloves1229@gmail.com>
2025-01-28 12:08:17 -0600

    update actions.ts
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,9 +4,7 @@ import sharp from 'sharp'
 export async function fetchImage(src: string) {
   const buffer = await fetch(src).then(async (res) => Buffer.from(await res.arrayBuffer()))
   try {
-    const resizedBuffer = await sharp(buffer)
-      .resize(75) // Resize to a smaller size
-      .toBuffer() // Convert to buffer
+    const resizedBuffer = await sharp(buffer).resize(10).blur(1).toBuffer() // Convert to buffer
     console.log(`Fetched image buffer data for ${src}: ${resizedBuffer.toString('base64')}`)
     return resizedBuffer
   } catch (error) {
