4e6348caba6c9bac7bb5a7654b4c345476b89c87
TheGloved1 <gloves1229@gmail.com>
2025-02-02 17:34:10 -0600

    update page.tsx
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function Page(): React.JSX.Element {
   return (
-    <SidebarProvider defaultOpen>
+    <SidebarProvider defaultOpen={false}>
       <ChatBot />
     </SidebarProvider>
   )
