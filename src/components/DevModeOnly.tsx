e6e568ba54abb4e746e00e7105382d12c39e9c88
TheGloved1 <gloves1229@gmail.com>
2025-02-08 22:07:04 -0600

    feat: :sparkles: Change QueryClient config
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/components/DevModeOnly.tsx
+++ src/components/DevModeOnly.tsx
@@ -5,7 +5,13 @@ import React from 'react'
 import ErrorAlert from './ErrorAlert'
 import Loading from './loading'
 
-export default function DevModeOnly({ children, fallback }: { children: React.ReactNode; fallback?: React.ReactNode }): React.JSX.Element {
+export default function DevModeOnly({
+  children,
+  fallback,
+}: {
+  children: React.ReactNode
+  fallback?: React.ReactNode
+}): React.JSX.Element {
   const isDev = useQuery({ queryKey: ['devMode'], queryFn: checkDevMode, initialData: false })
   if (isDev.isError) return <ErrorAlert>{isDev.error.message}</ErrorAlert>
   if (isDev.isFetching) return <Loading />
--- src/components/ReactQueryClientProvider.tsx
+++ src/components/ReactQueryClientProvider.tsx
@@ -3,6 +3,16 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React, { useState } from 'react'
 
 export default function ReactQueryClientProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: true,
+            experimental_prefetchInRender: true,
+          },
+        },
+      }),
+  )
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
