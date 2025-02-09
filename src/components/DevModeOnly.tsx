16f0bba4651ec25603ec37413a33eece853214d5
TheGloved1 <gloves1229@gmail.com>
2025-02-08 22:01:37 -0600

    refactor: :art: Remove useless type and add initialData
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/components/DevModeOnly.tsx
+++ src/components/DevModeOnly.tsx
@@ -5,13 +5,8 @@ import React from 'react'
 import ErrorAlert from './ErrorAlert'
 import Loading from './loading'
 
-type DevModeOnlyProps = {
-  children: React.ReactNode
-  fallback?: React.ReactNode
-}
-
-export default function DevModeOnly({ children, fallback }: DevModeOnlyProps): React.JSX.Element {
-  const isDev = useQuery({ queryKey: ['devMode'], queryFn: checkDevMode })
+export default function DevModeOnly({ children, fallback }: { children: React.ReactNode; fallback?: React.ReactNode }): React.JSX.Element {
+  const isDev = useQuery({ queryKey: ['devMode'], queryFn: checkDevMode, initialData: false })
   if (isDev.isError) return <ErrorAlert>{isDev.error.message}</ErrorAlert>
   if (isDev.isFetching) return <Loading />
   return <>{isDev.data ? children : fallback}</>
