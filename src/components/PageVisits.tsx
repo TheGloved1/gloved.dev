78e6075db5546049eae7c79d22475044f9ef9d36
TheGloved1 <gloves1229@gmail.com>
2025-02-15 06:29:08 -0600

    style: :art: Format code
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/components/PageVisits.tsx
+++ src/components/PageVisits.tsx
@@ -1,9 +1,9 @@
 'use client'
-import { apiRoute, fetchIp } from '@/lib/utils'
-import { useQuery } from '@tanstack/react-query'
 import { usePersistentState } from '@/hooks/use-persistent-state'
+import { apiRoute } from '@/lib/utils'
+import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const getVisits = async (visitorId: string | null) => {
   const encodedIp = encodeURIComponent(visitorId || '')
@@ -18,13 +18,16 @@ export function PageVisits(): React.JSX.Element | null {
   if (visitorId === null) {
     setVisitorId(crypto.randomUUID())
   }
-  const ipQuery = useQuery({ queryKey: ['pageVisits'], queryFn: () => getVisits(visitorId || null), initialData: { visitorIds: [], visits: 0 } })
+  const ipQuery = useQuery({
+    queryKey: ['pageVisits'],
+    queryFn: () => getVisits(visitorId || null),
+    initialData: { visitorIds: [], visits: 0 },
+  })
   if (ipQuery.isError) return null
   if (ipQuery.isFetching) return null
   return (
-    <div className='fixed bottom-0 items-center justify-center gap-4'>
+    <div className='fixed bottom-0 right-0 left-0 items-center justify-center gap-4'>
       <div className='text-center text-[0.5rem] sm:text-[0.6rem] md:text-[0.7rem] lg:text-[0.8rem]'>{`This page has been loaded ${ipQuery.data.visits} times by ${ipQuery.data.visitorIds.length} visitors`}</div>
     </div>
   )
 }
-
