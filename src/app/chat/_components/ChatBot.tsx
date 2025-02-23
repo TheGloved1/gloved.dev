41bfdad1ed244cc78740c8c698b4d1a5e27ab393
TheGloved1 <gloves1229@gmail.com>
2025-02-22 23:02:11 -0600

    refactor: :lipstick: Bring improved chat input from threads to root chat page
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/app/chat/_components/ChatBot.tsx
+++ src/app/chat/_components/ChatBot.tsx
@@ -4,19 +4,21 @@ import { createMessage, db } from '@/db'
 import { useIsMobile } from '@/hooks/use-mobile'
 import { usePersistentState } from '@/hooks/use-persistent-state'
 import { toast } from '@/hooks/use-toast'
-import { MessageSquare, Send } from 'lucide-react'
+import { Loader2, MessageSquare, Send } from 'lucide-react'
 import { redirect } from 'next/navigation'
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
 
 export default function ChatBot(): React.JSX.Element {
   const [input, setInput] = usePersistentState<string>('chatInput', '')
-  const messagesEndRef = useRef<HTMLDivElement>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [rows, setRows] = useState<number>(1)
   const isMobile = useIsMobile()
 
   const handleSubmit = async (
     e: React.FormEvent<HTMLFormElement> | React.KeyboardEvent<HTMLTextAreaElement>,
   ) => {
     e.preventDefault()
+    setLoading(true)
     const threadId = await db.createThread({
       title: 'Chat-' + Date.now().toString(),
     })
@@ -28,86 +30,71 @@ export default function ChatBot(): React.JSX.Element {
         description: 'Failed to generate message',
         variant: 'destructive',
       })
+      setLoading(false)
+      return
     }
+    setLoading(false)
     redirect('/chat/' + threadId.toString())
   }
 
+  const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.target.value)
+
+    const lineHeight = 24
+    const minRows = 1
+    const maxRows = 5
+    const newRows = Math.min(
+      Math.max(minRows, Math.floor(e.target.scrollHeight / lineHeight)),
+      maxRows,
+    )
+    setRows(newRows)
+  }
+
   return (
-    <div className='mx-auto max-w-7xl flex-1 p-4'>
-      <div className='space-y-4 pb-32'>
-        {/* {messages.map((m, index) => (
-          <div
-            key={index}
-            className={`flex ${m.role === Role.USER ? 'justify-end' : 'justify-start'}`}
-          >
-            <div
-              className={`group max-w-[75%] rounded-lg p-4 ${m.role === Role.USER ? 'bg-primary text-black' : 'bg-gray-800 text-white'
-                }`}
-            >
-              <div className='mb-2 flex items-center gap-2'>
-                {m.role === Role.USER ?
-                  <User2 className='h-4 w-4' />
-                  : <Bot className='h-4 w-4' />}
-                <span className='text-sm font-medium'>
-                  {m.role === Role.USER ? 'You' : 'AI'}
-                </span>
-                <CopyButton
-                  className='block size-4 text-sm text-gray-600 hover:block group-hover:block md:hidden'
-                  text={m.text}
-                  title='Copy raw message'
+    <>
+      <div className='absolute bottom-0 z-10 mx-auto flex w-full max-w-3xl flex-col text-center'>
+        <div className='fixed bottom-0 left-0 right-0 z-40 border-t border-gray-700 bg-gray-800 p-4'>
+          <form onSubmit={handleSubmit} className='container mx-auto max-w-4xl'>
+            <div className='flex items-center gap-2'>
+              <div className='relative flex-1'>
+                <MessageSquare className='absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 transform text-gray-400' />
+                <Textarea
+                  className='rounded-xl bg-gray-900 pl-10 text-gray-100'
+                  value={input}
+                  disabled={loading}
+                  placeholder={`Enter message here... ${isMobile ? '' : '(Shift + Enter for new line)'}`}
+                  rows={rows}
+                  aria-rowcount={rows}
+                  onChange={handleTextareaChange}
+                  onKeyDown={(e) => {
+                    // If Shift + Enter is pressed, add a new line
+                    if (e.key === 'Enter' && e.shiftKey) {
+                      e.preventDefault()
+                      setInput((prev) => prev + '\n')
+                    } else if (e.key === 'Enter') {
+                      if (!input.trim()) {
+                        e.preventDefault()
+                        return
+                      }
+                      handleSubmit(e)
+                    }
+                  }}
                 />
               </div>
-              {loading && (
-                <div className='flex items-center gap-2 text-white'>
-                  <Loader2 className='h-4 w-4 animate-spin' />
-                  <span className='text-sm'>Thinking...</span>
-                </div>
-              )}
-              <article
-                className={`prose max-w-none ${m.role === Role.USER ? 'prose-invert' : 'prose-invert prose-p:text-gray-100'}`}
+              <button
+                type='submit'
+                disabled={loading || !input}
+                className='btn bg-primary hover:bg-primary/90'
               >
-                <Markdown>{m.text}</Markdown>
-              </article>
+                {loading ?
+                  <Loader2 className='h-4 w-4 animate-spin' />
+                : <Send className='h-4 w-4' />}
+                <span className='sr-only'>Send</span>
+              </button>
             </div>
-          </div>
-        ))} */}
-        <div ref={messagesEndRef} />
+          </form>
+        </div>
       </div>
-      <div className='fixed bottom-0 left-0 right-0 z-50 border-t border-gray-700 bg-gray-800 p-4'>
-        <form onSubmit={handleSubmit} className='container mx-auto max-w-4xl'>
-          <div className='flex items-center gap-2'>
-            <div className='relative flex-1'>
-              <MessageSquare className='absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 transform text-gray-400' />
-              <Textarea
-                className='rounded-xl bg-gray-900 pl-10 text-gray-100'
-                value={input}
-                placeholder={`Enter message here... ${isMobile ? '' : '(Shift + Enter for new line)'}`}
-                onChange={(e) => setInput(e.target.value)}
-                onKeyDown={(e) => {
-                  if ((e.key === 'Enter' && e.shiftKey) || e.ctrlKey) {
-                    e.preventDefault()
-                    setInput((prev) => prev + '\n')
-                  } else if (e.key === 'Enter') {
-                    if (!input.trim()) {
-                      e.preventDefault()
-                      return
-                    }
-                    handleSubmit(e)
-                  }
-                }}
-              />
-            </div>
-            <button
-              type='submit'
-              disabled={!input.trim()}
-              className='btn bg-primary hover:bg-primary/90'
-            >
-              <Send className='h-4 w-4' />
-              <span className='sr-only'>Send</span>
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    </>
   )
 }
