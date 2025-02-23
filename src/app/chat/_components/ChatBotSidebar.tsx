9314af487be6a4e27376336a9719efb522471b31
TheGloved1 <gloves1229@gmail.com>
2025-02-23 12:33:45 -0600

    Improves chat sidebar
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/app/chat/_components/ChatBotSidebar.tsx
+++ src/app/chat/_components/ChatBotSidebar.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { Button } from '@/components/ui/button'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import {
   Sidebar,
@@ -11,7 +10,7 @@ import {
 import { db } from '@/db'
 import { useIsMobile } from '@/hooks/use-mobile'
 import { useLiveQuery } from 'dexie-react-hooks'
-import { MessageSquare, Plus, X } from 'lucide-react'
+import { MessageSquare, Plus, SquarePen, X } from 'lucide-react'
 import { Link } from 'next-view-transitions'
 import { redirect, useParams } from 'next/navigation'
 import React, { useState } from 'react'
@@ -33,11 +32,11 @@ export default function ChatBotSidebar({ children }: { children: React.ReactNode
 
   return (
     <SidebarProvider open={open} onOpenChange={setOpen}>
-      <Sidebar tw='m-0'>
+      <Sidebar variant='inset' className='m-0 border border-border'>
         <SidebarContent className='bg-gradient-to-bl from-[--background] to-[--background-secondary] rounded'>
           {/* <PageBack stayTop noFixed btnClassName='btn mt-2 w-fit bg-gray-700 hover:bg-gray-600' /> */}
           <div className='mt-2 w-fit'></div>
-          {threadId !== undefined && (
+          {isMobile && (
             <>
               <div className='divider divider-neutral text-gray-200'>New Chat</div>
               <SidebarGroup>
@@ -49,30 +48,42 @@ export default function ChatBotSidebar({ children }: { children: React.ReactNode
             </>
           )}
           <ScrollArea>
-            <div className='divider divider-neutral text-gray-200'>Chats</div>
+            <div className='divider divider-neutral text-gray-200'>
+              Chats{' '}
+              {!isMobile && (
+                <Link
+                  href='/chat'
+                  type='button'
+                  title='New chat'
+                  className='p-2 m-0 hover:bg-gray-500/50 rounded-lg'
+                >
+                  <SquarePen className='h-4 w-4' />
+                  <span className='sr-only'>New chat</span>
+                </Link>
+              )}
+            </div>
             {threads?.length ?
               threads.reverse().map((thread) => (
                 <div key={thread.id} className='p-2'>
                   <Link
                     key={thread.id}
                     href={`/chat/${thread.id}`}
-                    className={`my-0 flex items-center rounded-sm px-2 focus-within:outline-none focus-within:ring-[1px] focus-within:ring-[hsl(var(--ring))] hover:bg-[--background]/10 ${isThreadCurrent(thread.id) ? 'bg-[#2D2D2D]/50' : ''}`}
+                    className={`my-0 flex items-center rounded-sm px-2 focus-within:outline-none hover:bg-gray-500/10 focus-within:ring-[1px] focus-within:ring-[hsl(var(--ring))] hover:bg-[--background]/10 ${isThreadCurrent(thread.id) ? 'bg-gray-500/20' : ''}`}
                   >
-                    <div className='flex flex-1 flex-row gap-2 rounded-sm text-xs py-1 card items-center text-info-content group-data-[state=hover]:bg-[#2D2D2D]/50'>
+                    <div className='flex flex-1 flex-row gap-2 rounded-sm text-xs card items-center text-info-content group-data-[state=hover]:bg-[#2D2D2D]/50'>
                       <div
-                        className={`flex flex-1 flex-row gap-2 py-3 text-xs text-gray-200 ${isThreadCurrent(thread.id) ? 'font-bold cursor-default' : ''}`}
+                        className={`flex flex-1 flex-row gap-2 py-2 text-xs text-gray-200 ${isThreadCurrent(thread.id) ? 'font-bold cursor-default' : ''}`}
                       >
-                        <MessageSquare className='h-4 w-4' />
+                        <MessageSquare className='h-5 w-5' />
                         {thread.title}
                       </div>
-                      <Button
-                        variant='ghost'
-                        className='w-12 h-11 ml-auto md:opacity-0 transition-opacity duration-500 text-gray-500 md:group-hover:opacity-100 hover:text-red-800'
+                      <button
+                        className='w-12 h-12 pl-7 ml-auto md:opacity-0 transition-opacity duration-500 text-gray-500 md:group-hover:opacity-100 hover:text-red-800'
                         title='Delete Chat'
                         onClick={() => handleDelete(thread.id)}
                       >
-                        <X height={11} width={12} />
-                      </Button>
+                        <X height={16} width={16} />
+                      </button>
                     </div>
                   </Link>
                 </div>
