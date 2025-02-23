448038e7af8b5c47f1f417152b3b011cfca1675d
TheGloved1 <gloves1229@gmail.com>
2025-02-23 00:13:36 -0600

    refactor: :lipstick: Fix look of code blocks
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -10,7 +10,7 @@ type CodeBlockProps = {
 }
 const CodeBlock = ({ children = '', language = 'plaintext', props }: CodeBlockProps) => {
   return (
-    <div className='relative max-w-[92%]'>
+    <div className='relative max-w-full'>
       <div className='absolute inset-x-0 top-0 flex items-center justify-between border-foreground border h-10 px-4 py-2 text-xs bg-gray-900 rounded-t-lg text-muted-foreground'>
         <span className='font-mono'>{language}</span>
         <CopyButton text={String(children)} />
