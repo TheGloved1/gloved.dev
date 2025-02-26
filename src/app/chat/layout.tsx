f3458bb3ac932907cfe3fe23d08ceeb7361d0bef
TheGloved1 <gloves1229@gmail.com>
2025-02-26 11:36:14 -0600

    refactor: Update chat page metadata
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -17,8 +17,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: `https://${Constants.NAME}${Constants.ICON}`,
-        width: 500,
-        height: 500,
+        width: 400,
+        height: 400,
         alt: 'Logo',
       },
     ],
