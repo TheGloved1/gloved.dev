c91e2474be5217dd5434bce59d8ba996dee3bf1a
TheGloved1 <gloves1229@gmail.com>
2025-03-11 04:48:34 -0500

    Refines styling and adds Discord link
    Updates the fancy link styling for better visuals.
    
    Changes the background size and position, and adjusts text color.
    
    Adds a link to the Discord channel for viewing reels.
    
    Updates input placeholder.
    
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/app/sendreels/page.tsx
+++ src/app/sendreels/page.tsx
@@ -32,7 +32,7 @@ const ReelDownloader = () => {
           type='url'
           value={link}
           onChange={(e) => setLink(e.target.value.trim())}
-          placeholder='Enter Instagram Reel Link'
+          placeholder='Enter Link to Reel'
           required
         />
         <span className='flex w-full items-center justify-center gap-2'>
@@ -47,6 +47,17 @@ const ReelDownloader = () => {
           <Button type='submit' disabled={loading} className='p-4'>
             {loading ? 'Downloading...' : 'Send to Discord'}
           </Button>
+        </span>
+        <span className='text-sm text-gray-400'>
+          View reels on{' '}
+          <a
+            href='https://discord.com/channels/937806100546351174/1348939117190447136'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='fancy-link'
+          >
+            Discord
+          </a>
         </span>
       </form>
     </div>
