3a586be2b8443dbf12d2ea60a16a2fd17be34228
TheGloved1 <gloves1229@gmail.com>
2025-03-12 08:42:05 -0500

    Changes admin section layout to responsive grid
    Updates the admin section to use a responsive grid layout.
    The layout adjusts based on screen size.
    
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,7 +144,7 @@ export default function Page(): React.JSX.Element {
           <h2 className='text-3xl font-bold'>
             <u>{'Admin Stuff'}</u>
           </h2>
-          <div className='flex flex-col gap-4'>
+          <div className='grid grid-cols-1 gap-4 sm:grid-cols-1 md:gap-8'>
             <Link
               className='group flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20'
               href={'/admin'}
