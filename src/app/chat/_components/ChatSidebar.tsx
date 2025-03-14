4a507ebbbeda41c901887e0a535feede9e118557
TheGloved1 <gloves1229@gmail.com>
2025-03-14 00:52:14 -0500

    refactor: :lipstick: Improve thread delete button styling and remove useless component
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/app/chat/_components/ChatSidebar.tsx
+++ src/app/chat/_components/ChatSidebar.tsx
@@ -1,6 +1,16 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
@@ -10,15 +20,17 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from '@/components/ui/sidebar';
-import { dxdb } from '@/dexie';
+import { dxdb, Thread } from '@/dexie';
 import { useIsMobile } from '@/hooks/use-mobile';
+import { usePersistentState } from '@/hooks/use-persistent-state';
+import { tryCatch } from '@/lib/utils';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 import { useLiveQuery } from 'dexie-react-hooks';
-import { ChevronLeft, Home, MessageSquare, Plus, Settings, SquarePen } from 'lucide-react';
+import { ChevronLeft, Home, MessageSquare, Plus, Settings, SquarePen, X } from 'lucide-react';
 import { Link } from 'next-view-transitions';
 import { useParams, useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import DeleteAlert from './DeleteAlert';
+import React, { useCallback, useState } from 'react';
+import { toast } from 'sonner';
 
 export default function ChatBotSidebar({ children }: { children: React.ReactNode }) {
   const { threadId } = useParams();
@@ -26,8 +38,17 @@ export default function ChatBotSidebar({ children }: { children: React.ReactNode
   const isMobile = useIsMobile();
   const [open, setOpen] = useState(true);
   const threads = useLiveQuery(() => dxdb.getThreads(), [dxdb.threads], []);
+  const [lastThreadList, setLastThreadList] = usePersistentState<Thread[]>('lastThreadList', []);
 
-  const isCurrentThread = (id: string) => threadId === id;
+  const isCurrentThread = useCallback((id: string) => threadId === id, [threadId]);
+
+  const handleDelete = async (id: string) => {
+    const { error } = await tryCatch(dxdb.deleteThread(id)); // Delete the thread
+    if (error) return toast.error('Error deleting thread'), router.replace('/chat');
+    setLastThreadList(lastThreadList.filter((t) => t.id !== id));
+    toast.success('Thread deleted');
+    if (isCurrentThread(id)) router.replace('/chat');
+  };
 
   return (
     <SidebarProvider open={open} onOpenChange={setOpen}>
@@ -72,7 +93,7 @@ export default function ChatBotSidebar({ children }: { children: React.ReactNode
         <SidebarContent className='rounded bg-gradient-to-bl from-[--background] to-[--background-secondary]'>
           {threads?.length ?
             threads.map((thread) => (
-              <SidebarGroup key={thread.id} className='p-2'>
+              <SidebarGroup key={thread.id} className='group-chatbar peer overflow-x-hidden p-2'>
                 <Link key={thread.id} href={`/chat/${thread.id}`} title={thread.title}>
                   <SidebarGroupContent
                     className={`hover:bg-[--background]/10 my-0 flex h-14 max-h-14 cursor-pointer items-center rounded-sm px-2 focus-within:outline-none focus-within:ring-[1px] focus-within:ring-[hsl(var(--ring))] hover:bg-gray-500/10 ${isCurrentThread(thread.id) ? 'bg-gray-500/20' : ''}`}
@@ -93,7 +114,35 @@ export default function ChatBotSidebar({ children }: { children: React.ReactNode
                     </div>
                   </SidebarGroupContent>
                 </Link>
-                <DeleteAlert id={thread.id} isCurrentThread={isCurrentThread(thread.id)} />
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button
+                      className='group-chatbar-hover absolute right-0 top-1/2 h-12 w-12 -translate-y-1/2 pl-4 text-gray-500 transition-all duration-200 hover:bg-transparent hover:text-red-800 md:translate-x-3 md:opacity-0'
+                      variant='ghost'
+                      title='Delete Chat'
+                    >
+                      <X height={16} width={16} />
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className='sm:max-w-md'>
+                    <DialogHeader>
+                      <DialogTitle>Are you absolutely sure?</DialogTitle>
+                      <DialogDescription>
+                        This action cannot be undone. This will permanently delete this thread.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter className='pt-4'>
+                      <DialogClose asChild>
+                        <Button variant='ghost'>Cancel</Button>
+                      </DialogClose>
+                      <DialogClose asChild>
+                        <Button variant='destructive' onClick={() => handleDelete(thread.id)}>
+                          Delete
+                        </Button>
+                      </DialogClose>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
               </SidebarGroup>
             ))
           : null}
