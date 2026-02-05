c6980bc829ba678a9ebaf6e1dde05b1eb6b3c464
TheGloved1 <gloves1229@gmail.com>
2026-02-05 05:49:33 -0600

    feat(bgremover): Add OpenGraph metadata to layout
    Signed-off-by: TheGloved1 <gloves1229@gmail.com>

--- src/app/bgremover/layout.tsx
+++ src/app/bgremover/layout.tsx
@@ -5,6 +5,22 @@ export const metadata: Metadata = {
   title: `${Constants.NAME} | ${Constants.BGRemover.title}`,
   description: Constants.BGRemover.description,
   icons: '/bgremover.png',
+  openGraph: {
+    title: Constants.BGRemover.title,
+    description: Constants.BGRemover.description,
+    url: `https://${Constants.NAME}/bgremover`,
+    type: 'website',
+    siteName: Constants.NAME,
+    locale: 'en_US',
+    images: [
+      {
+        url: `https://${Constants.NAME}/bgremover.png`,
+        width: 400,
+        height: 400,
+        alt: 'Logo',
+      },
+    ],
+  },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
