317e14cb0c71a0ed3b8513d17c1237d1edf8fb2b
TheGloved1 <gloves1229@gmail.com>
2026-07-21 13:51:05 -0500

    Revert "fix(admin): add isAdmin debug logging"
    This reverts commit 94c0891e5e9a08ceb5bdc05e3cfa89825ab714af.

--- src/components/AdminShow.tsx
+++ src/components/AdminShow.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 export default function AdminShow({ children }: { children: React.ReactNode }): React.JSX.Element {
   const { user } = useUser();
   const isAdmin = user?.publicMetadata?.isAdmin === true;
-  console.log('isAdmin', isAdmin);
 
   if (isAdmin) return <>{children}</>;
   return <></>;
