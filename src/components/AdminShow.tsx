94c0891e5e9a08ceb5bdc05e3cfa89825ab714af
TheGloved1 <gloves1229@gmail.com>
2026-07-21 13:35:51 -0500

    fix(admin): add isAdmin debug logging

--- src/components/AdminShow.tsx
+++ src/components/AdminShow.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 export default function AdminShow({ children }: { children: React.ReactNode }): React.JSX.Element {
   const { user } = useUser();
   const isAdmin = user?.publicMetadata?.isAdmin === true;
+  console.log('isAdmin', isAdmin);
 
   if (isAdmin) return <>{children}</>;
   return <></>;
